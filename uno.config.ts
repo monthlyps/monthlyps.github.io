e425495b89c3d9536d921312841f6e67d7fd2293
jinsik park <pjshwa@gmail.com>
2024-10-30 05:14:45 +0900

    apply css class to user list ranks (#9)
    * apply css class to user list ranks
    
    * use shortcuts
    
    * remove getRankClass func
    
    * feat: apply rank style to scoreboard
    
    ---------
    
    Co-authored-by: kiwiyou <kiwiyou@kiwiyou.dev>

--- uno.config.ts
+++ uno.config.ts
@@ -2,14 +2,17 @@ import { defineConfig, presetUno } from "unocss"
 
 export default defineConfig({
   presets: [presetUno()],
+  shortcuts: {
+    "rank-1": "font-bold text-amber-5",
+    "rank-2": "font-bold text-bluegray-6 dark:text-bluegray-4",
+    "rank-3": "font-bold text-amber-7",
+    "theme-table":
+      "w-full [&_thead]:text-gray-4 [&_tr]:b-b [&_tr]:b-light-9 [&_td]:py-4 [&_td]:px-1 [&_td:first-child]:pl-2 dark:[&_tr]:b-dark-3 [&_tr]:transition [&_tr]:transition-property-[background-color] hover:[&_tr]:bg-light-3 hover:dark:[&_tr]:bg-dark-6",
+    hover: "transition-transform hover:translate-y-[-0.25rem]",
+  },
   theme: {
     fontFamily: {
       sans: 'Inter, "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif',
     },
-  },
-  shortcuts: {
-    "theme-table":
-      "w-full [&_thead]:text-gray-4 [&_tr]:b-b [&_tr]:b-light-9 [&_td]:py-4 [&_td]:px-1 [&_td:first-child]:pl-2 dark:[&_tr]:b-dark-3 [&_tr]:transition [&_tr]:transition-property-[background-color] hover:[&_tr]:bg-light-3 hover:dark:[&_tr]:bg-dark-6",
-    hover: "transition-transform hover:translate-y-[-0.25rem]",
   },
 })
