37bca3bae043e56b594ec7bbc3e167a3dd358baf
kiwiyou <kiwiyou@portone.io>
2024-10-30 22:07:12 +0900

    feat: search user

--- uno.config.ts
+++ uno.config.ts
@@ -4,11 +4,13 @@ export default defineConfig({
   presets: [presetUno()],
   shortcuts: {
     "rank-1": "font-bold text-amber-5",
-    "rank-2": "font-bold text-bluegray-6 dark:text-bluegray-4",
+    "rank-2": "font-bold text-bluegray-6 dark:text-muted",
     "rank-3": "font-bold text-amber-7",
     "theme-table":
       "w-full [&_thead]:text-gray-4 [&_tr]:b-b [&_tr]:b-light-9 [&_td]:py-4 [&_td]:px-1 [&_td:first-child]:pl-2 dark:[&_tr]:b-dark-3 [&_tr]:transition [&_tr]:transition-property-[background-color] hover:[&_tr]:bg-light-3 hover:dark:[&_tr]:bg-dark-6",
     hover: "transition-transform hover:translate-y-[-0.25rem]",
+    "b-border": "b-light-9 dark:b-dark-3",
+    "text-muted": "text-gray-4",
   },
   theme: {
     fontFamily: {
